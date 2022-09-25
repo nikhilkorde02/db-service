e624180e8e6a6375e71ddf45eea5cebae5f8b47c
nkorde <nikhil.korde@capgemini.com>
2022-09-25 22:09:53 +0530

    Remove commented code

--- src/main/java/com/dbservice/dbservice/model/Quotes.java
+++ src/main/java/com/dbservice/dbservice/model/Quotes.java
@@ -9,21 +9,7 @@ public class Quotes {
 	private String userName;
 	private List<String> quotes;
 
-	//Karthik -> AIRTEL,JIO,SBI,RELIANCE,SUZLON
 
-	//Quotes quotes=new Quotes('kartik','AIRTEL,JIO,SBI,RELIANCE,SUZLON');
-
-	//1 kartik AIRTEL
-	//2 kartik JIO
-	//3 kartik SBI
-	//4 kartik RELIANCE
-	//5 kartik SUZLON
-
-
-	//for each (String: qutoes.getQuotes())
-	//{
-	// quotedao.save('karthink',String);
-	//}
 	public Quotes() {
 	}
 
